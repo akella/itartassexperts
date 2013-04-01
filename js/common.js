ea5e2d7e9aba5553ab55323cef5436ae671e5128
Artur <morozarturv@gmail.com>
2013-04-01 17:09:23 +0300

    all fixes

--- js/common.js
+++ js/common.js
@@ -1,92 +1,64 @@
 $(document).ready(function() {
 
-//datepicker
-if ($('.datepicker-container').length > 0) {  
-  $('.datepicker table tr:last').hide();  
-  $('.form__period').click(function() {
-    if (!$(this).hasClass('active')) {
-      $(this).addClass('active');
-      $(this).next().fadeIn();
-      $(this).next().next().show();
-      var current = new Date();
-      if (current.getMonth() == 0){
-        current.setMonth(11);
-        current.setFullYear(current.getFullYear()-1);
-      } else {
-        current.setMonth(current.getMonth()-1);
-      }
-      $(this).next().children('.datepicker-container').DatePicker({
-        flat: true,
-        format: 'd B Y',
-        date: [],
-        current: current,
-        calendars: 3,
-        mode: 'range',
-        starts: 1,
-        onChange: function(formated) {
-          $(this).parent().next().children('.datepicker-detail__date').html('<strong>' + formated.join('</strong> – <strong>') + '</strong>');
+//slider general
+if ($('.js-sl').length > 0) {
+  $('.js-sl').each(function() {
+    var slider_prev = $(this).prev().children().children('.js-sl-prev');
+    var slider_next = $(this).prev().children().children('.js-sl-next');
+    $(this).cycle({ 
+      fx: 'scrollHorz',
+      timeout: 0,
+      speed: 300,
+      prev: slider_prev,
+      next: slider_next, 
+      nowrap: 1,
+      after: function(curr, next, opts, fwd) {
+        var height_wrap = $(this).height();        
+        $(this).parent().animate({height: height_wrap}, 300); 
+        var height_wrap = $(this).height();
+        var index = opts.currSlide;
+        var counter = opts.slideCount;
+        if (index == 0) {
+          slider_prev.addClass('disabled');
         }
-      });
-    }
-    else {
-
-    }
-  });   
-  $('.datepicker-detail__btn button').click(function() {
-    var date_value = $(this).parent().prev().html();
-    $(this).parent().parent().parent().prev().html(date_value);
-    $(this).parent().parent().parent().fadeOut();
-    $(this).parent().parent().parent().prev().removeClass('active');
-    $(this).parent().parent().parent().next().fadeOut(); 
-    $(this).parent().parent().parent().prev().addClass('selected');
-    return false;
-  });
-  $('.datepicker-detail__btn a').click(function() {
-    if (!$(this).parent().parent().parent().prev().hasClass('selected')) {
-      $(this).parent().parent().parent().fadeOut();
-      $(this).parent().parent().parent().prev().removeClass('active');
-      $(this).parent().parent().parent().next().fadeOut();
-      var date_begin = $(this).parent().parent().parent().prev().html();
-      $(this).parent().prev().html(date_begin);    
-      $('.datepicker-container').DatePickerClear();
-    }
-    else {
-      $(this).parent().parent().parent().fadeOut();
-      $(this).parent().parent().parent().prev().removeClass('active');
-      $(this).parent().parent().parent().next().fadeOut();      
-    }
-    return false;
-  });
-  $('.form__datepicker-close').click(function() {
-    var date_begin1 = $(this).prev().prev().html();
-    $(this).prev().children('.datepicker-detail').children('.datepicker-detail__date').html(date_begin1);
-    $('.form__datepicker').fadeOut();
-    $('.form__period').removeClass('active');
-    $('.datepicker-container').DatePickerClear();
-    $(this).hide();
+        else {
+          slider_prev.removeClass('disabled');
+        };
+        if (counter - 1 == index) {
+          slider_next.addClass('disabled');
+        }
+        else {
+          slider_next.removeClass('disabled');
+        };
+      }
+    });
   });
 };
+$('.js-sl-count .js-sl-prev').click(function() {
+  if (!$(this).hasClass('disabled')) {
+    var count = $(this).next().children('span').html();
+    count = parseInt(count);
+    count--;
+    $(this).next().children('span').html(count);
+  };
+});
+$('.js-sl-count .js-sl-next').click(function() {
+  if (!$(this).hasClass('disabled')) {
+    var count = $(this).prev().children('span').html();
+    count = parseInt(count);
+    count++;
+    $(this).prev().children('span').html(count);
+  };
+});
 
-//messages
-$('.js-message button').click(function() {
-  $(this).parent().slideUp(100);
+//enter
+$('.js-enter-open').click(function() {
+  $('.popup_enter, .popup-bg').fadeIn();
   return false;
 });
-$('.js-sites').click(function() {
-  if ($(this).hasClass('open')) {
-    $(this).removeClass('open');
-    $(this).parent().next().slideUp();
-  }
-  else {
-    $(this).addClass('open');
-    $(this).parent().next().slideDown();
-  };  
+$('.js-enter-close, .popup-bg').click(function() {
+  $('.popup_enter, .popup-bg').fadeOut();
+  return false;
 });
-
-//sortable + draglable
-if ($('.js-form-sort').length > 0) {
-  $('.js-form-sort').sortable({axis: 'y', cursor: 'move'});
-  $('.js-form-sort').disableSelection();
-};
 
 });
